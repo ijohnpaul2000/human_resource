b63249d3ce44023e742556b9de2448574e34d8a4
John Paulo A. Geronimo <77067773+geronimium-gr@users.noreply.github.com>
2022-08-29 15:30:44 +0800

    dashboard ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,117 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 const Dashboard = () => {
-  return <div className="">Dashboard</div>;
+  return (
+    <div className="">
+      <div className="header">Dashboard</div>
+      <div className="flex flex-wrap mt-6 -mx-3">
+        <div className="m-4 w-80">
+          <div className="after:opacity-95 after:bg-gradient-to-tl after:from-cyan-900 after:to-cyan-800 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']">
+            <div className="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center"></div>
+            <div className="relative z-20 flex-auto w-full p-4 text-left text-white">
+              <div className="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
+                <i
+                  className="top-0 z-10 text-transparent ni leading-none ni-diamond text-lg bg-gradient-to-tl from-slate-900 to-slate-800 bg-clip-text opacity-100"
+                  aria-hidden="true"
+                >
+                  <div className="font-bold text-2xl">0</div>
+                </i>
+              </div>
+              <div className="transition-all duration-200 ease-nav-brand">
+                <h6 className="mb-4 font-bold text-white">Total Applicants</h6>
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/"
+                  className="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-xs hover:shadow-soft-2xl hover:scale-102"
+                >
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 w-80">
+          <div className="after:opacity-95 after:bg-gradient-to-tl after:from-emerald-900 after:to-emerald-800 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']">
+            <div className="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center"></div>
+            <div className="relative z-20 flex-auto w-full p-4 text-left text-white">
+              <div className="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
+                <i
+                  className="top-0 z-10 text-transparent ni leading-none ni-diamond text-lg bg-gradient-to-tl from-slate-900 to-slate-800 bg-clip-text opacity-100"
+                  aria-hidden="true"
+                >
+                  <div className="font-bold text-2xl">0</div>
+                </i>
+              </div>
+              <div className="transition-all duration-200 ease-nav-brand">
+                <h6 className="mb-4 font-bold text-white">
+                  Employee with Employments
+                </h6>
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/"
+                  className="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-xs hover:shadow-soft-2xl hover:scale-102"
+                >
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 w-80">
+          <div className="after:opacity-95 after:bg-gradient-to-tl after:from-yellow-900 after:to-yellow-800 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']">
+            <div className="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center"></div>
+            <div className="relative z-20 flex-auto w-full p-4 text-left text-white">
+              <div className="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
+                <i
+                  className="top-0 z-10 text-transparent ni leading-none ni-diamond text-lg bg-gradient-to-tl from-slate-900 to-slate-800 bg-clip-text opacity-100"
+                  aria-hidden="true"
+                >
+                  <div className="font-bold text-2xl">0</div>
+                </i>
+              </div>
+              <div className="transition-all duration-200 ease-nav-brand">
+                <h6 className="mb-4 font-bold text-white">User Registration</h6>
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/"
+                  className="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-xs hover:shadow-soft-2xl hover:scale-102"
+                >
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 w-80">
+          <div className="after:opacity-95 after:bg-gradient-to-tl after:from-red-900 after:to-red-800 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']">
+            <div className="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center"></div>
+            <div className="relative z-20 flex-auto w-full p-4 text-left text-white">
+              <div className="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
+                <i
+                  className="top-0 z-10 text-transparent ni leading-none ni-diamond text-lg bg-gradient-to-tl from-slate-900 to-slate-800 bg-clip-text opacity-100"
+                  aria-hidden="true"
+                >
+                  <div className="font-bold text-2xl">0</div>
+                </i>
+              </div>
+              <div className="transition-all duration-200 ease-nav-brand">
+                <h6 className="mb-4 font-bold text-white">
+                  Total Deployed Employee
+                </h6>
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/"
+                  className="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-xs hover:shadow-soft-2xl hover:scale-102"
+                >
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
