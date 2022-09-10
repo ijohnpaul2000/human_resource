8f33dca5182f1f82b1a7d1102b2c43f4925ddc90
John Paulo A. Geronimo <77067773+geronimium-gr@users.noreply.github.com>
2022-09-10 21:07:15 +0800

    Downgrade yung Alien ni Ben 10
    *ba dum tss*
    
    THE FOLLOWING LINE OF CODES (COMMENTED) ARE FUNCTIONING AND UPDATED
    
    * Sidebar.jsx
    * sidebarData.js

--- client/src/data/sidebarData.js
+++ client/src/data/sidebarData.js
@@ -1,18 +1,30 @@
+/*
+  THE FOLLOWING LINE OF CODES (COMMENTED) ARE FUNCTIONING AND UPDATED
+*/
+
+// export const sidebarData = [
+//   {
+//     title: "Applicant Screening",
+//     path: "/applicant/screening",
+//   },
+//   {
+//     title: "Applicant Requirements",
+//     path: "/applicant/requirements",
+//   },
+//   {
+//     title: "Applicant Appointment",
+//     path: "/applicant/appointment",
+//   },
+//   {
+//     title: "Contract Signing",
+//     path: "/applicant/contract",
+//   },
+// ];
+
+// WILL BE PRESENTED TO THE PANEL
 export const sidebarData = [
   {
     title: "Applicant Screening",
     path: "/applicant/screening",
-  },
-  {
-    title: "Applicant Requirements",
-    path: "/applicant/requirements",
-  },
-  {
-    title: "Applicant Appointment",
-    path: "/applicant/appointment",
-  },
-  {
-    title: "Contract Signing",
-    path: "/applicant/contract",
   },
 ];
