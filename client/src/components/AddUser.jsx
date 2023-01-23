452a86547ec778bd097d1ea3ace38a1d4558f2b6
John Paulo Geronimo <fx99@ph.sunlife>
2023-01-23 23:35:57 +0800

    defect: Password double encryption

--- client/src/components/AddUser.jsx
+++ client/src/components/AddUser.jsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { BsChevronRight } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
-import { SET_SELECTED_USER, USER_INFO, getUsersInfo, IS_MODAL_OPENED } from "../redux/features/userReducer";
+import {
+  SET_SELECTED_USER,
+  USER_INFO,
+  getUsersInfo,
+  IS_MODAL_OPENED,
+} from "../redux/features/userReducer";
 import { useFormik } from "formik";
 import axios from "axios";
 import { notifyToast } from "../helpers/notifyToast";
 import { ToastContainer } from "react-toastify";
-import { Checkbox } from 'primereact/checkbox';
-import { validationSchema } from "../yupUtils/comp/UserYup";
-
+import { Checkbox } from "primereact/checkbox";
+import { Chip } from "primereact/chip";
+import * as Yup from "yup";
 
 const AddUser = () => {
-
   // useState for Checkbox
   const [checked, setChecked] = useState(false);
 
   //Redux
   const dispatch = useDispatch();
-  const { isModalOpened, userInfo, selectedUser } =
-    useSelector((store) => store.users);
-  
+  const { isModalOpened, userInfo, selectedUser } = useSelector(
+    (store) => store.users
+  );
 
   // Field initial values
   const initialValues = {
@@ -29,9 +33,20 @@ const AddUser = () => {
     lastname: selectedUser ? selectedUser?.lastname : "",
     username: selectedUser ? selectedUser?.username : "",
     password: selectedUser ? selectedUser?.password : "",
-    user_level: selectedUser ? selectedUser?.user_level : "",
+    user_level: selectedUser ? selectedUser?.user_level : "user",
     email: selectedUser ? selectedUser?.email : "",
   };
+
+  // Validations
+  const validationSchema = Yup.object().shape({
+    firstname: Yup.string().required("First Name is required"),
+    middlename: Yup.string().required("Middle Name is required"),
+    lastname: Yup.string().required("Last Name is required"),
+    username: Yup.string().required("Username is required"),
+    password: checked == true ? Yup.string().required("Password is required") : Yup.string().notRequired(),
+    user_level: Yup.string().required("User Level is required"),
+    email: Yup.string().required("Email is required"),
+  });
 
   // To initialize formik
   const formik = useFormik({
@@ -39,20 +54,6 @@ const AddUser = () => {
     validationSchema,
     enableReinitialize: true,
     onSubmit: async (values, { resetForm }) => {
-    
-      // try {
-      //   const response = await axios.post(
-      //     "http://localhost:5000/api/users/",
-      //     data
-      //   );
-      //   console.log(response);
-      //   notifyToast("User Added", "success");
-
-      //   resetForm();
-      // } catch (error) {
-      //   notifyToast(error.response.data.message, "error");
-      // }
-
       try {
         const response = await axios({
           method: selectedUser ? "PUT" : "POST",
@@ -64,10 +65,7 @@ const AddUser = () => {
         resetForm();
         dispatch(getUsersInfo());
         console.log(response);
-        notifyToast(
-          selectedUser ? "User Updated" : "User Added",
-          "success"
-        );
+        notifyToast(selectedUser ? "User Updated" : "User Added", "success");
         dispatch(SET_SELECTED_USER(""));
         reCloseModal();
       } catch (error) {
@@ -90,7 +88,7 @@ const AddUser = () => {
 
   // Error Checker
   const isFieldValid = (fieldName) =>
-  formik.touched[fieldName] && formik.errors[fieldName];
+    formik.touched[fieldName] && formik.errors[fieldName];
 
   //Error Message
   const getErrorMessage = (error) => {
@@ -109,209 +107,238 @@ const AddUser = () => {
   /* 
     In every change na nangyayari sa site (addition ng data, updating ng data) lagi tong magrurun. okay lang naman boss kasfksdfka PINANIS NI POL
   */
-    useEffect(() => {
-      dispatch(getUsersInfo());
+  useEffect(() => {
+    dispatch(getUsersInfo());
 
-      if (selectedUser === "") {
-        setChecked(true);
-      }
+    if (selectedUser === "") {
+      setChecked(true);
+    }
+  }, [dispatch]);
 
-      // if (checked === false) {
-      //   delete initialValues.password;
-      // }
-    }, [dispatch]);
-
-
-    return (
-      <div>
-        <div className="block p-6 rounded-lg shadow-lg bg-white">
-          <form className="w-full" onSubmit={formik.handleSubmit}>
-          {/* First Name to Last Name */}
-            <div className="flex flex-wrap -mx-3 mt-2 mb-2 gap-y-2">
-              <div className="form-group w-full px-3 md:w-1/3">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  id="firstname"
-                  placeholder="Enter first name"
-                  className={
-                    isFieldValid("firstname")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.firstname}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("firstname")}
-              </div>
-
-              <div className="form-group w-full px-3 md:w-1/3">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  Middle Name
-                </label>
-                <input
-                  type="text"
-                  id="middlename"
-                  placeholder="Enter middle name"
-                  className={
-                    isFieldValid("middlename")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.middlename}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("middlename")}
-              </div>
-
-              <div className="form-group w-full px-3 md:w-1/3">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  id="lastname"
-                  placeholder="Enter lastname"
-                  className={
-                    isFieldValid("lastname")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.lastname}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("lastname")}
-              </div>
-            </div>
-
-          {/* Username to Role */}
-            <div className="flex flex-wrap -mx-3 mt-2 mb-2 gap-y-2">
-              <div className="form-group w-full px-3 md:w-1/3">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  User Name
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="Enter username"
-                  className={
-                    isFieldValid("username")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.username}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("username")}
-              </div>
-
-              <div className="form-group w-full px-3 md:w-1/3">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Enter email"
-                  className={
-                    isFieldValid("email")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("email")}
-              </div>
-
-              <div className="form-group w-full px-3 md:w-1/4">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  User Role
-                </label>
-
-                <div className="relative">
-                  <select
-                    type="text"
-                    id="user_level"
-                    placeholder="Enter user level"
-                    className={
-                      isFieldValid("user_level")
-                        ? "border-2 border-red-600 select-decorator"
-                        : "select-decorator"
-                    }
-                    value={formik.values.user_level}
-                    onChange={formik.handleChange}
-                  >
-                    <option value="admin">Admin</option>
-                    <option value="super_user">Super User</option>
-                    <option value="user">Staff</option>
-                    <option value="applicant">Applicant</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
-                    <BsChevronRight />
-                  </div>
-                </div>
-                {getErrorMessage("user_level")}
-              </div>
-
-              {/* For updating password */}
-              <div className="field-checkbox" style={selectedUser ? {display: "block"} : {display: "none"}}>
-                <Checkbox inputId="binary" className="mr-2 ml-3" checked={checked} onChange={e => setChecked(e.checked)} />
-                <label htmlFor="binary">Change Password</label>
-              </div>
-
-              <div className="form-group w-full px-3" style={checked ? {display: "block"} : {display: "none"}}>
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Enter password"
-                  className={
-                    isFieldValid("password")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("password")}
-              </div>
-
-              {/* <div className="form-group w-full px-3">
-                <label className="form-label inline-block mb-2 text-gray-700 font-bold">
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Confirm password"
-                  className={
-                    isFieldValid("password")
-                      ? "border-2 border-red-600 formFields"
-                      : "formFields"
-                  }
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                />
-                {getErrorMessage("password")}
-              </div> */}
-
-            </div>
-            <button
-              type="submit"
-              className="form-btn disabled:opacity-50 enabled:hover:bg-pink-400"
-              disabled={!(formik.dirty && formik.isValid)}
-            >
-              Submit
-          </button>
-          </form>
+  return (
+    <div>
+      <div className="block p-6 rounded-lg shadow-lg bg-white">
+        <div className="text-center" style={
+          !selectedUser
+            ? { visibility: "hidden", display: "none" }
+            : { visibility: "visible" }
+        }>
+          <img
+            src={`https://api.multiavatar.com/${selectedUser.firstname}.svg`}
+            className="rounded-full w-32 mb-4 mx-auto"
+            alt="Avatar"
+          />
+          <div className="flex justify-center">
+            <Chip
+              label={`${selectedUser.firstname} ${selectedUser.middlename} ${selectedUser.lastname}`}
+              icon="pi pi-user"
+              className="mr-2 mb-2 custom-chip"
+            />
+            <Chip
+              label={selectedUser.username}
+              icon="pi pi-at"
+              className="mr-2 mb-2"
+            />
+          </div>
+          <div className="flex justify-center">
+            <Chip
+              label={selectedUser.email}
+              icon="pi pi-envelope"
+              className="mr-2 mb-2"
+            />
+          </div>
         </div>
+
+        <form className="w-full" onSubmit={formik.handleSubmit}>
+          {/* First Name to Last Name */}
+          <div
+            className="flex flex-wrap -mx-3 mt-2 mb-2 gap-y-2"
+            style={
+              selectedUser
+                ? { visibility: "hidden", display: "none" }
+                : { visibility: "visible" }
+            }>
+            <div className="form-group w-full px-3 md:w-1/3">
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                First Name
+              </label>
+              <input
+                type="text"
+                id="firstname"
+                placeholder="Enter first name"
+                className={
+                  isFieldValid("firstname")
+                    ? "border-2 border-red-600 formFields"
+                    : "formFields"
+                }
+                value={formik.values.firstname}
+                onChange={formik.handleChange}
+              />
+              {getErrorMessage("firstname")}
+            </div>
+
+            <div className="form-group w-full px-3 md:w-1/3">
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                Middle Name
+              </label>
+              <input
+                type="text"
+                id="middlename"
+                placeholder="Enter middle name"
+                className={
+                  isFieldValid("middlename")
+                    ? "border-2 border-red-600 formFields"
+                    : "formFields"
+                }
+                value={formik.values.middlename}
+                onChange={formik.handleChange}
+              />
+              {getErrorMessage("middlename")}
+            </div>
+
+            <div className="form-group w-full px-3 md:w-1/3">
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="lastname"
+                placeholder="Enter lastname"
+                className={
+                  isFieldValid("lastname")
+                    ? "border-2 border-red-600 formFields"
+                    : "formFields"
+                }
+                value={formik.values.lastname}
+                onChange={formik.handleChange}
+              />
+              {getErrorMessage("lastname")}
+            </div>
+          </div>
+
+          {/* Username to Email */}
+          <div className="flex flex-wrap -mx-3 mt-2 mb-2 gap-y-2" style={
+            selectedUser
+              ? { visibility: "hidden", display: "none" }
+              : { visibility: "visible" }
+          }>
+            <div className="form-group w-full px-3 md:w-1/2">
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                User Name
+              </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="Enter username"
+                className={
+                  isFieldValid("username")
+                    ? "border-2 border-red-600 formFields"
+                    : "formFields"
+                }
+                value={formik.values.username}
+                onChange={formik.handleChange}
+              />
+              {getErrorMessage("username")}
+            </div>
+
+            <div className="form-group w-full px-3 md:w-1/2">
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter email"
+                className={
+                  isFieldValid("email")
+                    ? "border-2 border-red-600 formFields"
+                    : "formFields"
+                }
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+              {getErrorMessage("email")}
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mt-2 mb-2 gap-y-2">
+
+            <div className="form-group w-full px-3">
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                User Role
+              </label>
+
+              <div className="relative">
+                <select
+                  type="text"
+                  id="user_level"
+                  placeholder="Enter user level"
+                  className={
+                    isFieldValid("user_level")
+                      ? "border-2 border-red-600 select-decorator"
+                      : "select-decorator"
+                  }
+                  value={formik.values.user_level}
+                  onChange={formik.handleChange}>
+                  <option value="admin">Admin</option>
+                  <option value="super_user">Super User</option>
+                  <option value="user">Staff</option>
+                  <option value="applicant">Applicant</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+                  <BsChevronRight />
+                </div>
+              </div>
+              {getErrorMessage("user_level")}
+            </div>
+
+            {/* For updating password */}
+            <div
+              className="field-checkbox"
+              style={selectedUser ? { display: "block" } : { display: "none" }}
+            >
+              <Checkbox
+                inputId="binary"
+                className="mr-2 ml-3"
+                checked={checked}
+                onChange={(e) => setChecked(e.checked)}
+              />
+              <label htmlFor="binary">Change Password</label>
+            </div>
+
+            <div
+              className="form-group w-full px-3"
+              style={checked ? { display: "block" } : { display: "none" }}
+            >
+              <label className="form-label inline-block mb-2 text-gray-700 font-bold">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter password"
+                className={
+                  isFieldValid("password")
+                    ? "border-2 border-red-600 formFields"
+                    : "formFields"
+                }
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+              {getErrorMessage("password")}
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="form-btn disabled:opacity-50 enabled:hover:bg-pink-400"
+            disabled={!(formik.dirty && formik.isValid)}
+          >
+            Submit
+          </button>
+        </form>
       </div>
-    );
-  };
-  
-  export default AddUser;
+    </div>
+  );
+};
+
+export default AddUser;
